2e3f4a14f2006201da3c3153758632eccb754080
Jack Kelly <jackmkelly519@gmail.com>
2026-01-07 12:45:48 -0500

    new home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
+import "./Home.css";
+import princetonLogo from "./images/princetonLogo.png";
+import bnuLogo from "./images/bnuLogo.png";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className='title-container'>
+        <div className='title'>Welcome</div>
+        <div className='subtitle'>I'm Jack Kelly! Get to know a little bit more about me below.</div>
+      </div>
+      <div className='header-container'>
+        <h1><b>Projects</b></h1>
+      </div>
+      <div className='header-container'>
+        <h1><b>Affiliations</b></h1>
+        <img src={princetonLogo}></img>
+        <img src={bnuLogo}></img>
+      </div>
+      <div className='header-container'>
+        <h1><b>Socials</b></h1>
+        <Link to="https://www.linkedin.com/in/jack-kelly-391696259/">LinkedIn</Link>
+        <Link to="https://www.instagram.com/jackmkelly519/">Instagram</Link>
+      </div>
+    </>
+  );
 }
